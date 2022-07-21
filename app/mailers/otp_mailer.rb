e369a3d34eb46e833e591c7c3169770ac4264124
Aftab Iqbal <aftab.iqbal@tecaudex.com>
2022-07-21 17:05:40 +0500

    otp mailer remove

--- app/mailers/otp_mailer.rb
+++
@@ -1,7 +0,0 @@
-class OtpMailer < ApplicationMailer
-  def otp_send
-    otp = SecureRandom.random_number(1000)
-    @user = params[:user]
-    mail(to: @user.email, subject: "OTP: #{otp}")
-  end
-end
