da9f72498709d5b62a75bd1463d02d5e1c29c4d5
aftab iqbal <aftab.iqbal@tecaudex.com>
2022-08-13 23:30:42 +0500

    rswag_removed_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   # devise_for :users
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  # mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Api::Engine => '/api-docs'
   # mount_devise_token_auth_for 'User', at: 'auth'
   # mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
